27888d45fdd4c4447ff94495ef01610d04305fc4
fernando_cuestas <fernandocuestas886@gmail.com>
2020-05-22 02:47:54 -0300

    ahh

---
+++ index.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+
+mongoose.connect(config.db, (err, res) => {
+  if (err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
+  console.log('Conexión a la base de datos establecida...')
+
+  app.listen(config.port, () => {
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+    console.log(`API REST corriendo en http://localhost:${config.port}`)
+  })
+})
